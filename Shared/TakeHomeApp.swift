d4f7674b5f02a4b3a35dbd22076cd350351ac56f
Ryan Cochrane <ryancochrane@icloud.com>
2022-07-27 15:07:45 -0400

    Initial Commit

---
+++ Shared/TakeHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TakeHomeApp.swift
+//  Shared
+//
+//  Created by Ryan Cochrane on 7/27/22.
+//
+
+import SwiftUI
+
+@main
+struct TakeHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
